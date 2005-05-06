2247a53333dd4d18a51fe6ef261ef5891f5d570c
stefan <stefan>
2005-05-06 14:47:53 +0000

    lib/rant/import/subfile.rb: Allow basedir argument, fixes.

--- lib/rant/import/subfile.rb
+++ lib/rant/import/subfile.rb
@@ -7,18 +7,32 @@ require 'rant/rantlib'
 
 class Rant::Generators::SubFile
     def self.rant_generate(rac, ch, args, &block)
-	unless args.size == 1
-	    rac.abort_at(ch, "SubFile takes one argument.")
+	case args.size
+	when 1
+	    basedir, fine = nil, args.first
+	    path = fine
+	when 2
+	    basedir, fine = args
+	    path = File.join(basedir, fine)
+	else
+	    rac.abort_at(ch, "SubFile takes one or two arguments.")
 	end
 	file_desc = rac.pop_desc
-	rac.prepare_task(args.first, block, ch) { |name,pre,blk|
-	    dir, file = File.split name
+	rac.prepare_task(path, block, ch) { |name,pre,blk|
+	    dir, file = File.split(fine.to_s)
+	    dirp = basedir ? File.join(basedir, dir) : dir
 	    unless dir == "."
-		dt = rac.resolve(dir)
+		dt = rac.resolve(dirp)
 		if dt.empty?
-		    dt = rac.cx.gen(::Rant::Generators::Directory, dir)
+		    dt = [if basedir
+			rac.cx.gen(
+			    ::Rant::Generators::Directory, basedir, dir)
+		    else
+			rac.cx.gen(
+			    ::Rant::Generators::Directory, dir)
+		    end]
 		end
-		pre << dt
+		pre.concat(dt)
 	    end
 	    rac.cx.desc file_desc
 	    ::Rant::FileTask.new(rac, name, pre, &blk)
--- test/import/subfile/test_subfile.rb
+++ test/import/subfile/test_subfile.rb
@@ -26,4 +26,66 @@ class TestSubFile < Test::Unit::TestCase
 	assert(test(?d, "sub2.t"))
 	assert(!test(?e, "sub2.t/file"))
     end
+    def test_fail_no_basedir
+	assert_rant(:fail, "sub3.t/file")
+	assert(!test(?e, "sub3.t"))
+	assert(!test(?e, "sub3.t/file"))
+    end
+    def test_basedir
+	FileUtils.mkdir "sub3.t"
+	assert_rant("sub3.t/file")
+	assert(test(?d, "sub3.t"))
+	assert(test(?f, "sub3.t/file"))
+    end
+    def test_dirtask_exists
+	assert_rant("sub.t/file2")
+	assert(test(?d, "sub.t"))
+	assert(test(?f, "sub.t/file2"))
+	assert(!test(?e, "sub.t/file"))
+    end
+    def test_make_two
+	assert_rant("sub.t/file", "sub.t/file2")
+	assert(test(?f, "sub.t/file"))
+	assert(test(?f, "sub.t/file2"))
+    end
+    def test_two_dirs
+	assert_rant("sub4.t/sub/file")
+	assert(test(?f, "sub4.t/sub/file"))
+    end
+    def test_basedir_two_dirs
+	FileUtils.mkdir "sub5.t"
+	out, err = assert_rant("sub5.t/sub/sub/file")
+	assert(!out.strip.empty?)
+	assert(test(?f, "sub5.t/sub/sub/file"))
+	out, err = assert_rant("sub5.t/sub/sub/file")
+	assert(out.strip.empty?)
+    end
+    def test_make_dir
+	assert_rant("sub.t")
+	assert(test(?d, "sub.t"))
+	assert(!test(?e, "sub.t/file"))
+    end
+    def test_only_file
+	assert_rant("file.t")
+	assert(test(?f, "file.t"))
+	out, err = assert_rant("file.t")
+	assert(out.strip.empty?)
+    end
+    def test_autoclean
+	assert_rant("-fautoclean.rf", "sub.t/file")
+	assert(test(?f, "sub.t/file"))
+	FileUtils.mkdir "sub2.t"
+	assert_rant("-fautoclean.rf", "sub2.t/sub.t/file")
+	assert(test(?f, "sub2.t/sub.t/file"))
+	FileUtils.mkdir "sub3.t"
+	assert_rant("-fautoclean.rf", "sub3.t/file")
+	assert(test(?f, "sub3.t/file"))
+	assert_rant("-fautoclean.rf", "autoclean")
+	assert(test(?d, "sub2.t"))
+	assert(test(?d, "sub3.t"))
+	%w(sub.t sub2.t/sub.t sub3.t/file).each { |f|
+	    assert(!test(?e, f),
+		"#{f} should have been unlinked by AutoClean")
+	}
+    end
 end
