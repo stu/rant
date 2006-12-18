cbb61f2b12cc1b2d22257264338505ffce16ba18
russel <russel>
2006-12-18 20:30:18 +0000

    Committed up the first version of the LaTeX processing support bits and pieces.

---
+++ lib/rant/import/latex.rb
@@ -0,0 +1,159 @@
+#  Rant support for LaTeX processing
+#
+#  Copyright © 2006 Russel Winder
+#
+#  This program is free software; you can redistribute it and/or modify it under the terms of
+#  the GNU General Public License as published by the Free Software Foundation; either version 2,
+#  or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+#  the GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along with this program; if
+#  not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+#  02111-1307 USA
+
+#  Various bits and pieces for processing LaTeX source from a Rantfile.
+#
+#  Author:: Russel Winder
+#  Copyright:: Copyright © 2006 Russel Winder
+#  Licence:: GPL
+
+require 'rant/rantlib'
+
+module Rant
+
+  #def self.init_import_latex( rac , *rest )
+  #  p rac
+  #end
+
+  module LaTeX
+  
+    LtxExtension = '.ltx'
+    TexExtension = '.tex'
+
+    DviExtension = '.dvi'
+    EpsExtension = '.eps'
+    PdfExtension = '.pdf'
+    PsExtension = '.ps'
+
+    AuxExtension = '.aux'
+    BblExtension = '.bbl'
+    BlgExtension = '.blg'
+    IdxExtension = '.idx'
+    IlgExtension = '.ilg'
+    IndExtension = '.ind'
+    LogExtension = '.log'
+    TocExtension = '.toc'
+    PdfBookMarkExtension = '.out'
+
+    CleanListExtensions = [
+                           AuxExtension , DviExtension , LogExtension , TocExtension ,
+                           BblExtension , BlgExtension ,
+                           IdxExtension , IlgExtension , IndExtension ,
+                           PdfBookMarkExtension
+                          ]
+
+    # The options to the various commands -- there must be a neater way of doing this.
+
+    $bibtexOptions = '-min-crossrefs=999'
+    $makeindexOptions = '-c'
+    $dvipsOptions = '-D 1200'
+    $ps2pdfOptions = '-sPAPERSIZE=a4 -dPDFSETTINGS=/printer'
+
+    #  Run the LaTeX or pdfLaTeX command (second parameter true means pdfLaTeX otherwise LaTeX) sufficient
+    #  time to ensure the resulting file (DVI or PDF depending on whether LaTeX or pdfLaTeX is being used.) 
+    #  is correct.  If there are BibTeX references then run BibTeX appropriately.  If there are index files
+    #  then run makeindex appropriately.
+
+    def LaTeX.runLaTeX( root , use_pdfLaTeX = false )
+      if File.exists?( root + LtxExtension ) then source = root + LtxExtension
+      elsif File.exists?( root + TexExtension ) then source = root + LtxExtension
+      else raise Exception.new( "Neither #{root}.ltx or #{root}.tex exist." ) end
+      doLaTeX = proc { system( ( use_pdfLaTeX ? 'pdflatex' : 'latex' ) + ' ' + source ) }
+      conditionallyDoLaTeX = proc {
+        rerun = File.open( root + LogExtension ) { | file | file.read.index( /(Warning:.*Rerun|Warning:.*undefined citations)/ ) != nil }
+        if rerun then doLaTeX.call end
+        rerun
+      }
+      doLaTeX.call
+      bibTeXRun = false
+      Dir.glob( root + '.*.aux' ).each { | file | system( "bibtex #{$bibtexOptions} #{file}" ) ; bibTeXRun = true }
+      if File.open( root + AuxExtension ) { | file | file.read.index( 'bibdata' ) != nil }
+        system( "bibtex #{$bibtexOptions} #{root}#{AuxExtension}" )
+        bibTeXRun = true
+      end
+      if bibTeXRun
+        doLaTeX.call
+        if conditionallyDoLaTeX.call then conditionallyDoLaTeX.call end
+      end
+      makeindexRun = false
+      Dir.glob( root + '*.idx' ).each { | file | system( "makeindex #{$makeindexOptions} #{file}" ) ; makeindexRun = true }
+      if makeindexRun then doLaTeX.call end
+      doLaTeX.call
+      if conditionallyDoLaTeX.call
+        if ! conditionallyDoLaTeX.call
+          raise Exception.new( '#### Something SERIOUSLY Wrong. ###' )
+        end
+      end
+    end
+
+    #  As the method name says, create a PostScript file from a DVI file.  This currently uses dvips a fact
+    #  that must be known to the LaTeX source code to ensure the correct driver is used to create the DVI
+    #  file.
+
+    def LaTeX.createPsFromDvi( root )
+      system( "dvips #{$dvipsOptions} -o #{root}#{PsExtension} #{root}#{DviExtension}" )
+    end
+
+    #  As the method name says, create a PDF file from a DVI file.  This currently uses dvips and then
+    #  ps2pdf a fact that must be known to the LaTeX source code to ensure the correct driver is used to
+    #  create the DVI file.
+
+    def LaTeX.createPdfFromDvi( root )
+      system( "dvips #{$dvipsOptions} -Ppdf -G0 -o #{root}#{PsExtension} #{root}#{DviExtension}" )
+      system( "ps2pdf #{ps2pdfOptions} #{root}#{PsExtension}" )
+    end
+
+  end # module LaTeX
+
+  class Generators::LaTeX
+    
+    def self.rant_gen ( rant , ch , args , &block )
+      if args == [] then raise ArgumentError , 'Must provide source file name as parameter.' end
+      sourceFile = args.shift
+      if args != [] then properties = args.shift else properties = {} end
+      targetExtension = case properties[ 'generate' ]
+                        when 'ps' , 'PS' , 'postscript' , 'Postscript' , 'PostScript'
+                          Rant::LaTeX::PsExtension
+                        else
+                          Rant::LaTeX::PdfExtension
+                        end
+      sourceExtension = File.extname( sourceFile )
+      root = sourceFile.sub( sourceExtension , '' )
+      target = root + targetExtension
+      dependencies = [ sourceFile ]
+      if properties[ 'parts' ] then dependencies += properties[ 'parts' ] end
+      rant.desc "Create #{target}."
+      rant.file target => dependencies do
+        case targetExtension
+        when Rant::LaTeX::PdfExtension
+          Rant::LaTeX.runLaTeX( root , true )
+        when Rant::LaTeX::PsExtension
+          Rant::LaTeX.runLaTeX( root , false )
+          Rant::LaTeX.createPsFromDvi( root )
+        else
+          raise ArgumentError , "#{targetExtension} is not a valid extension to the LaTeX generator." 
+        end
+      end
+      begin
+        Rant::LaTeX::CleanListExtensions.each { | ext | rant.var[:clean].include root + ext }
+      rescue
+      end
+      target
+    end
+
+  end # class Generators::LaTeX
+
+end # module Rant
---
+++ test/import/latex/test_latex.rb
@@ -0,0 +1,173 @@
+#  Test for the LaTeX generator for Rant.
+#
+#  Copyright © 2006 Russel Winder
+#
+#  This program is free software; you can redistribute it and/or modify it under the terms of
+#  the GNU General Public License as published by the Free Software Foundation; either version 2,
+#  or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+#  the GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along with this program; if
+#  not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+#  02111-1307 USA
+
+require 'test/unit'
+require 'tutil'
+
+$testImportCommandDir ||= File.expand_path(File.dirname(__FILE__))
+
+class TestLaTeX < Test::Unit::TestCase
+
+  include Rant::TestUtil
+
+  SourceFileName = 'file.ltx'
+  PdfTarget = SourceFileName.sub( 'ltx' , 'pdf' )
+  PsTarget = SourceFileName.sub( 'ltx' , 'ps' )
+  RantfileName = 'Rantfile'
+  DefaultTarget = ''
+  ErrorTarget = 'flobadob'
+
+  NoSourceRantfile = "import 'latex'\ngen LaTeX"
+  MissingSourceRantfile = "import 'latex'\ngen LaTeX , 'blah.ltx'"
+  NoOptionsSourceRantfile = "import 'latex'\ngen LaTeX , '#{SourceFileName}'"
+  PdfOptionSourceRantfile = "import 'latex'\ngen LaTeX , '#{SourceFileName}' , { 'generate' => 'PDF' }"
+  PsOptionSourceRantfile = "import 'latex'\ngen LaTeX , '#{SourceFileName}' , { 'generate' => 'PS' }"
+
+  NoSourceErrorMessage = 'Must provide source file name as parameter.'
+
+  def cantMakeErrorMessage( target ) ; "Don't know how to make `#{target}'" end
+
+  def setup
+    Dir.chdir( $testImportCommandDir )
+  end
+
+  def teardown
+    Dir.chdir( $testImportCommandDir )
+    Rant::Sys.rm_f( RantfileName )
+    Rant::Sys.rm_f( Rant::LaTeX::CleanListExtensions.map { | extension | SourceFileName.sub( '.ltx' , extension ) } + [ PdfTarget , PsTarget ] )
+  end
+
+  def test_noSourceDefault
+    Rant::TestUtil.write_to_file( RantfileName , NoSourceRantfile )
+    out , err = assert_rant( :fail , DefaultTarget )
+    assert( err.include?( NoSourceErrorMessage ) )
+  end
+
+  def test_noSourcePdfTarget
+    Rant::TestUtil.write_to_file( RantfileName , NoSourceRantfile )
+    out , err = assert_rant( :fail , PdfTarget )
+    assert( err.include?( NoSourceErrorMessage ) )
+  end
+
+  def test_noSourcePsTarget
+    Rant::TestUtil.write_to_file( RantfileName , NoSourceRantfile )
+    out , err = assert_rant( :fail , PsTarget )
+    assert( err.include?( NoSourceErrorMessage ) )
+  end
+
+  def test_noSourceErrorTarget
+    Rant::TestUtil.write_to_file( RantfileName , NoSourceRantfile )
+    out , err = assert_rant( :fail , ErrorTarget )
+    assert( err.include?( NoSourceErrorMessage ) )
+  end
+
+  def test_missingSourceDefault
+    Rant::TestUtil.write_to_file( RantfileName , MissingSourceRantfile )
+    out , err = assert_rant( :fail , DefaultTarget )
+    assert( err.include?( cantMakeErrorMessage( DefaultTarget ) ) )
+  end
+
+  def test_missingSourcePdfTarget
+    Rant::TestUtil.write_to_file( RantfileName , MissingSourceRantfile )
+    out , err = assert_rant( :fail , PdfTarget )
+    assert( err.include?( cantMakeErrorMessage( PdfTarget ) ) )
+  end
+
+  def test_missingSourcePsTarget
+    Rant::TestUtil.write_to_file( RantfileName , MissingSourceRantfile )
+    out , err = assert_rant( :fail , PsTarget )
+    assert( err.include?( cantMakeErrorMessage( PsTarget ) ) )
+  end
+
+  def test_missingSourceErrorTarget
+    Rant::TestUtil.write_to_file( RantfileName , MissingSourceRantfile )
+    out , err = assert_rant( :fail , ErrorTarget )
+    assert( err.include?( cantMakeErrorMessage( ErrorTarget ) ) )
+  end
+
+  def test_noOptionsDefault
+    Rant::TestUtil.write_to_file( RantfileName , NoOptionsSourceRantfile )
+    out , err = assert_rant( :fail , DefaultTarget )
+    assert( err.include?( cantMakeErrorMessage( DefaultTarget ) ) )
+  end
+
+  def test_noOptionsPdfTarget
+    Rant::TestUtil.write_to_file( RantfileName , NoOptionsSourceRantfile )
+    out , err = assert_rant( PdfTarget )
+    assert( File.exists?( PdfTarget ) )
+  end
+
+  def test_noOptionsPsTarget
+    Rant::TestUtil.write_to_file( RantfileName , NoOptionsSourceRantfile )
+    out , err = assert_rant( :fail , PsTarget )
+    assert( err.include?( "Don't know how to make `#{PsTarget}'" ) )
+  end
+
+  def test_hoOptionsErrorTarget
+    Rant::TestUtil.write_to_file( RantfileName , NoOptionsSourceRantfile )
+    out , err = assert_rant( :fail , ErrorTarget )
+    assert( err.include?( "Don't know how to make `#{ErrorTarget}'" ) )
+  end
+
+  def test_pdfOptionDefault
+    Rant::TestUtil.write_to_file( RantfileName , PdfOptionSourceRantfile )
+    out , err = assert_rant( :fail , DefaultTarget )
+    assert( err.include?( cantMakeErrorMessage( DefaultTarget ) ) )
+  end
+
+  def test_pdfOptionPdfTarget
+    Rant::TestUtil.write_to_file( RantfileName , PdfOptionSourceRantfile )
+    out , err = assert_rant( PdfTarget )
+    assert( File.exists?( PdfTarget ) )
+  end
+
+  def test_pdfOptionPsTarget
+    Rant::TestUtil.write_to_file( RantfileName , PdfOptionSourceRantfile )
+    out , err = assert_rant( :fail , PsTarget )
+    assert( err.include?( "Don't know how to make `#{PsTarget}'" ) )
+  end
+
+  def test_pdfOptionErrorTarget
+    Rant::TestUtil.write_to_file( RantfileName , PdfOptionSourceRantfile )
+    out , err = assert_rant( :fail , ErrorTarget )
+    assert( err.include?( "Don't know how to make `#{ErrorTarget}'" ) )
+  end
+
+  def test_psOptionDefault
+    Rant::TestUtil.write_to_file( RantfileName , PsOptionSourceRantfile )
+    out , err = assert_rant( :fail , DefaultTarget )
+    assert( err.include?( cantMakeErrorMessage( DefaultTarget ) ) )
+  end
+
+  def test_psOptionPdfTarget
+    Rant::TestUtil.write_to_file( RantfileName , PsOptionSourceRantfile )
+    out , err = assert_rant( :fail , PdfTarget )
+    assert( err.include?( "Don't know how to make `#{PdfTarget}'" ) )
+  end
+
+  def test_psOptionPsTarget
+    Rant::TestUtil.write_to_file( RantfileName , PsOptionSourceRantfile )
+    out , err = assert_rant( PsTarget )
+    assert( File.exists?( PsTarget ) )
+  end
+
+  def test_psOptionErrorTarget
+    Rant::TestUtil.write_to_file( RantfileName , PsOptionSourceRantfile )
+    out , err = assert_rant( :fail , ErrorTarget )
+    assert( err.include?( "Don't know how to make `#{ErrorTarget}'" ) )
+  end
+
+end
